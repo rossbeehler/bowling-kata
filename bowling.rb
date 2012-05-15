a33f085cdf55e28da7a5298731cd3057306e54ee
Ross Beehler <ross@sipp.net>
2012-05-15 16:57:41 -0400

    Added partial game scoring

--- bowling.rb
+++ bowling.rb
@@ -1,22 +1,29 @@
 class Bowling
+  def initialize
+    @rolls = []
+    @roll_ptr = 0
+  end
+
   def roll(pins)
-    @rolls ||= []
     @rolls << pins 
   end
 
   def score
     score = 0
-    @start_of_frame = 0
-    10.times do
+    for_each_frame do
       score += next_two_balls if open_frame?
       score += next_three_balls if closed_frame?
-      @start_of_frame += strike? ? 1 : 2
+      move_to_start_of_next_frame!
     end
     score
   end
 
+  def for_each_frame(&block)
+    10.times(&block)
+  end
+    
   def next_two_balls
-    @rolls[@start_of_frame] + @rolls[@start_of_frame+1]
+    pins_at(@roll_ptr) + pins_at(@roll_ptr+1)
   end
 
   def open_frame?
@@ -24,14 +31,23 @@ class Bowling
   end
 
   def next_three_balls
-    next_two_balls + @rolls[@start_of_frame+2]
+    next_two_balls + pins_at(@roll_ptr+2)
   end
 
   def closed_frame?
     !open_frame?
   end
 
+  def pins_at(index)
+    return 0 if index >= @rolls.size
+    @rolls[index]
+  end
+
+  def move_to_start_of_next_frame!
+      @roll_ptr += strike? ? 1 : 2
+  end
+
   def strike?
-    @rolls[@start_of_frame] == 10
+    pins_at(@roll_ptr) == 10
   end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -16,34 +16,69 @@ def roll_strike
 end
 
 describe Bowling,"score" do
-  it "returns 0 when rolling all gutter balls" do
-    roll_many(20, 0)
-    subject.score.should == 0
+  context "complete games" do
+    it "returns 0 when rolling all gutter balls" do
+      roll_many(20, 0)
+      subject.score.should == 0
+    end
+
+    it "returns 20 when rolling all ones" do
+      roll_many(20, 1)
+      subject.score.should == 20
+    end
+
+    it "should handle one spare" do
+      roll_spare
+      subject.roll(3)
+      roll_many(17, 0)
+      subject.score.should == 16
+    end
+
+    it "should handle one strike" do
+      roll_strike 
+      subject.roll(3)
+      subject.roll(4)
+      roll_many(16, 0)
+      subject.score.should == 24
+    end
+
+    it "should return 300 on a perfect game" do
+      roll_many(12, 10)
+      subject.score.should == 300
+    end
   end
 
-  it "returns 20 when rolling all ones" do
-    roll_many(20, 1)
-    subject.score.should == 20
-  end
+  context "partial games" do
+    it "starts at 0" do
+      subject.score.should == 0
+    end
 
-  it "should handle one spare" do
-    roll_spare
-    subject.roll(3)
-    roll_many(17, 0)
-    subject.score.should == 16
-  end
+    it "scores first throw" do
+      subject.roll(3)
+      subject.score.should == 3
+    end
 
-  it "should handle one strike" do
-    roll_strike 
-    subject.roll(3)
-    subject.roll(4)
-    roll_many(16, 0)
-    subject.score.should == 24
-  end
-
-  it "should return 300 on a perfect game" do
-    roll_many(12, 10)
-    subject.score.should == 300
+    it "scores first open frame" do
+      subject.roll(3)
+      subject.roll(4)
+      subject.score.should == 7
+    end
+    
+    it "does not count gutter-5-5 as a spare" do
+      subject.roll(0)
+      subject.roll(5)
+      subject.roll(5)
+      subject.roll(1)
+      subject.score.should == 11
+    end
+    
+    it "does not count gutter-10 as a strike" do
+      subject.roll(0)
+      subject.roll(10)
+      subject.roll(3)
+      subject.roll(4)
+      subject.score.should == 20
+    end
   end
 end
 
